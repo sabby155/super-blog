948465eccdfb10e7942bd7140020d335c9cce2c5
sabby155 <nebula155@gmail.com>
2019-04-10 08:44:47 -0400

    fix create, add flash and error messages, create show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum:5, maximum: 50} 
-    validates :descriptions, presence: true,length: {minimum:10, maximum: 500}
+    validates :description, presence: true, length: {minimum:10, maximum: 500}
 end     
